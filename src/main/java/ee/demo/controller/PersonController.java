b7633655dd3c0969143bebe723b774bc7d0c36a6
Annemarii Hunt <annemarii.hunt@parnu.ee>
2018-12-16 13:22:05 +0200

    Removed unnecessary files and dependencies

--- src/main/java/ee/demo/controller/PersonController.java
+++ src/main/java/ee/demo/controller/PersonController.java
@@ -1,26 +1,17 @@
 package ee.demo.controller;
-
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ee.demo.model.Person;
 import ee.demo.repository.PersonRepository;
-import ee.demo.service.PersonService;
+
 
 @RestController
 @RequestMapping("/person")
@@ -28,8 +19,7 @@ import ee.demo.service.PersonService;
 
 public class PersonController {
 
-  @Autowired 
-  PersonService personService;
+
   
   @Autowired 
   PersonRepository personRepository;
--- src/main/java/ee/demo/repository/PersonRepository.java
+++ src/main/java/ee/demo/repository/PersonRepository.java
@@ -11,7 +11,7 @@ public interface PersonRepository extends MongoRepository<Person, String> {
 	public Person findOneById(String id);
 	public Person findOneBysocialSecurityId(String socialSecurityId);
 	public List<Person> findByFirstName(String firstName);
-	//public Person updateUser(String firstName, String flastName) ;
+	
 
 
 }
